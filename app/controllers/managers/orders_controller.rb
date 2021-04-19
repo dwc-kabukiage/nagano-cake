a20f1d878efd9518e2493bce45e3c5c35bb7a89a
nomu0719 <kei53415@gmail.com>
2021-04-19 09:24:02 +0000

    管理者側の注文詳細作りました

--- app/controllers/managers/orders_controller.rb
+++ app/controllers/managers/orders_controller.rb
@@ -1,2 +1,22 @@
 class Managers::OrdersController < ApplicationController
+    
+    def index
+        @order = Order.all
+    end
+    def show
+        @order = Order.find(params[:id])
+    end
+    def update 
+        @order = Order.find(params[:id])
+        if @order.update(order_params)
+            flash[:success] = "変更しました"
+            redirect_to managers_order_path(@order)
+        else
+            render "show"
+        end
+    end
+    private
+    def order_params
+       params.require(:order).permit(:received_status)
+    end
 end
--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -11,13 +11,13 @@ class Members::SessionsController < Devise::SessionsController
     if @member
       if (@member.valid_password?(params[:member][:password]) && (@member.active_for_authentication? == false))
         flash[:error] = "退会済みです。"
-        redirect_to root_path
+        redirect_to new_customer_session_path
       end
     else
       flash[:error] = "必須項目を入力してください。"
     end
   end
-end
+
   # GET /resource/sign_in
   # def new
   #   super
