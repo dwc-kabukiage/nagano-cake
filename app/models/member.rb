1d650fbb496d934a8c3545b9f1476ccd4f1c546a
EC2 Default User <ec2-user@ip-172-31-83-158.ec2.internal>
2021-04-21 06:41:06 +0000

    delivery

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,7 +5,7 @@ class Member < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :orders, dependent: :destroy
-
+  has_many :deliveries, dependent: :destroy
   #ログインする時に退会済み(is_deleted==true)のユーザーを弾くためのメソッド  
   #ログインしようとしているuserがにんしょうかのうかどうか -> true or false;
   #user.is_deletedがfalseのときにtrueをかえす.
